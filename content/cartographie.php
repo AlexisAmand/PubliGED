bd3156129845c307c72bd31e15715f20d84c913a
Alex <alexis.amand@gmail.com>
2018-06-29 17:40:02 +0200

    Essai d'export en pdf avec la librairie mPDF

--- content/cartographie.php
+++ content/cartographie.php
@@ -66,7 +66,24 @@
 		
 	?>
 	
-	];       
+	];
+
+	/* test de marqueurs perso */
+	
+	var greenIcon = L.icon({
+    iconUrl: 'img/icon.jpg',
+    shadowUrl:'img/icon.jpg',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+	});
+	
+	
+	/* fin du test */
+		       
 
 	/* ajout des points sur la carte */
 	
@@ -74,7 +91,7 @@
 	var i;
 	for (i = 0; i < points.length; i++) 
 		{
-		marker[i] = new L.Marker([points[i][0], points[i][1]], {url: points[i][2]});
+		marker[i] = new L.Marker([points[i][0], points[i][1]], {url: points[i][2]}, {icon: greenIcon});
 	    marker[i].addTo(mymap);
 	    marker[i].on('click', onMapClick);
 	    };
