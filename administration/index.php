4598e37cdf6232bc3722d23652b6b4dd4e60b7cc
Alex <alexis.amand@gmail.com>
2019-07-02 12:35:26 +0200

    Font Awesome de la partie admin passe à la version 5.9.0

--- administration/index.php
+++ administration/index.php
@@ -18,8 +18,10 @@ include('../langues/admin.php');
 
   <title>SB Admin 2 - Dashboard</title>
 
-  <!-- Custom fonts for this template-->
+  <!-- Font Awesome 5.9.0 -->
   <link href="css/fontawesome/css/all.min.css" rel="stylesheet" type="text/css"> 
+  
+  <!-- Custom fonts for this template -->	
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
