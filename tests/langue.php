fca735d66c14a403013de3a2dc12beffb0861efb
Alex <alexis.amand@gmail.com>
2019-07-26 15:56:54 +0200

    La langue est includée selon celle du navigateur

--- tests/langue.php
+++ tests/langue.php
@@ -6,18 +6,6 @@
 /* ------------------------------------------------------- */
 
 $language = explode(',' , $_SERVER['HTTP_ACCEPT_LANGUAGE']);
-
-switch($language[0])
-	{
-	case 'fr':
-		include('langues/'.$language[0].'.php');
-		break;
-	case 'en':
-		include('langues/'.$language[0].'.php');
-		break;
-	default:
-		include('langues/'.$language[0].'.php');
-		break;
-	}
+include('langues/'.$language[0].'.php');
 
 ?>
