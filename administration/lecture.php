b42dc84bf80fff25b81201a38286051a7b854f65
alexisamand <alexis.amand@gmail.com>
2021-05-20 16:58:38 +0200

    [bugfix] résolution du bug sur la constante READ_GEDCOM

--- administration/lecture.php
+++ administration/lecture.php
@@ -110,7 +110,7 @@ include ('../class/class.php');
 		          
           <div class="card shadow mb-4">
                <div class="card-header py-3">
-                 <h6 class="m-0 font-weight-bold text-primary"><?php echo READ_GEDCOM; ?></h6>
+                 <h6 class="m-0 font-weight-bold text-primary"><?php echo ADM_GED_READ; ?></h6>
                </div>
                <div class="card-body">
     
