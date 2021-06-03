7a7a4e53a63438f8b59779ff0865805da73a6a75
alexisamand <alexis.amand@gmail.com>
2021-06-03 13:52:36 +0200

    [maj] Message de validation des options

--- administration/options.php
+++ administration/options.php
@@ -103,9 +103,8 @@ include('../langues/admin/fr.php');
         
         <!-- Page Heading -->
           <h1 class="h3 mb-2 text-gray-800"><?php echo ADM_ST_TITLE; ?></h1>
-          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
           
-          <!-- DataTales Example -->         
+          <!-- DataTales Example -->
           <div class="card shadow mb-4">
                <div class="card-header py-3">
                  <h6 class="m-0 font-weight-bold text-primary"><?php echo ADM_ST_RUBRIC_1; ?></h6>
