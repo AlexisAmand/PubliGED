212d9fcfd592d90ca3c9b039d62c43830fa4fc59
Alex <alexis.amand@gmail.com>
2022-04-27 15:42:48 +0200

    [bugfix] datatables est en français sur le frontoffice

--- js/datatables.js
+++ js/datatables.js
@@ -1,3 +1,7 @@
-$(document).ready( function () {
-    $('#dataTable').DataTable();
+$(document).ready(function() {
+    $('#dataTable').DataTable( {
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.11.5/i18n/fr-FR.json'
+        }
+    } );
 } );
